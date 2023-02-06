a7fe89712479ffa13f6126ad138c801d6197b950
unknown <wisejohn950330@gmail.com>
2023-02-06 17:59:19 +0900

    Priority Queue

---
+++ Priority Queue/baekjoon_최대 힙.py
@@ -0,0 +1,17 @@
+# 백준 11279번
+
+import sys
+import heapq
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(heap, (-num))
+    else:
+        try:
+            print(-1 * heapq.heappop(heap))
+        except:
+            print(0)
